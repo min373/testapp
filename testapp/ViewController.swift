fef8576672650ae62fc0b66ff880f96d59b10bc3
Minami <ym_mnm_373@yahoo.co.jp>
2018-12-11 03:11:21 +0900

    Initial Commit

---
+++ testapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  testapp
+//
+//  Created by 山本南 on 2018/12/11.
+//  Copyright © 2018 min373. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
